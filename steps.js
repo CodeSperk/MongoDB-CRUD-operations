3477876815067309ab788cd528caa6d345c55397
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-04-18 13:41:06 +0600

    created mongoDB connection, upadated steps  and index.js

--- steps.js
+++ steps.js
@@ -4,5 +4,25 @@
     --> "start" : "node index.js" (Script at package.json)
     --> npm i express cors mongodb
     --> index.js with basic express setup including cors & middleware
-    
+    --> node_modules in the .gitignore file
+ */
+
+
+/**
+ * MongoDB Connection: (MongoDB Basic project at : ahsanugps@gmail.com )
+    --> Create a database user & save user name and password
+    --> Choose a connection Methods : 
+        Drivers > 1.Node.js 3. connection string to index.js
+    --> update  add userName & password in index.js.
+    --> comment 'await client.close()'
+    --> Quick start: 
+        1. Update user name & password if needed
+        2. Add Ip address in access list if needed
+        3. Finish and close
+    --> Database Access: you can add new user or edit. 
+    --> Network Access: ADD IP ADDRESS > Allow access from anywhere
+    --> Database : browser collection >
+          * you can delete default database and collections 
+          * not necessary: you can add custom database and collection 
+    --> 
  */
