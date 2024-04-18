9060f74d80905c17fb8b2f115a65f8f11853fb40
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-04-18 12:59:25 +0600

    Server site setup including index.js file with express, cors

---
+++ steps.js
@@ -0,0 +1,8 @@
+/**
+ * Install server side: 
+    --> npm init -y
+    --> "start" : "node index.js" (Script at package.json)
+    --> npm i express cors mongodb
+    --> index.js with basic express setup including cors & middleware
+    
+ */
